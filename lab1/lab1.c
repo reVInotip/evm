83ebb51a3dcb275530c8fce5bcb6f667cb4db40f
reVInotip <grigoriy.novikov220@gmail.com>
2023-09-29 11:58:50 +0700

    lab_passed

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,8 +1,10 @@
-#include <assert.h>
+#include <bits/time.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/times.h>
+#include <unistd.h>
 
 enum {
     REDAING_ERROR = -1,
@@ -27,10 +29,21 @@ double MonteKarlo(const int countPoints) {
 }
 
 int main() {
+    /*struct tms start, end;
+    const long clocks_per_sec = sysconf(_SC_CLK_TCK);
+    long clocks;
+    times(&start);*/
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     const int countPoints = INT_MAX / 4;
     
     const double pi = MonteKarlo(countPoints);
     printf("%lf\n", pi);
-
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    printf("Time taken %lf sec.", end.tv_sec - start.tv_sec + 0.000000001*(end.tv_nsec-start.tv_nsec));
+    /*times(&end);
+    clocks = end.tms_utime - start.tms_utime;
+    printf("Time taken: %lf sec.\n",
+        (double)clocks / clocks_per_sec);*/
     return EXIT_SUCCESS;
 }
